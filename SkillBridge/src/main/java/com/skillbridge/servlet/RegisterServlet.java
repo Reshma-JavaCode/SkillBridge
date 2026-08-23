1753d4b84341a9d4eec9f364a6fd7622d491b9ef
Reshma-Javacode <Reshma.mohd2508@gmail.com>
2026-08-23 23:21:16 +0530

    Validations

--- SkillBridge/src/main/java/com/skillbridge/servlet/RegisterServlet.java
+++ SkillBridge/src/main/java/com/skillbridge/servlet/RegisterServlet.java
@@ -27,7 +27,70 @@ public class RegisterServlet extends HttpServlet {
 
 		
 		System.out.println(username+" : "+firstname+" : "+lastname);
-		System.out.println(email+" : "+mobile+" : "+password);
+		System.out.println(email+" : "+mobile);
+		
+		//Server side Validations: Check empty fields
+		if (username == null || username.trim().isEmpty()) {
+		    System.out.println("Username is required");
+		    return;
+		}
+
+		if (firstname == null || firstname.trim().isEmpty()) {
+		    System.out.println("First name is required");
+		    return;
+		}
+
+		if (lastname == null || lastname.trim().isEmpty()) {
+		    System.out.println("Last name is required");
+		    return;
+		}
+
+		if (email == null || email.trim().isEmpty()) {
+		    System.out.println("Email is required");
+		    return;
+		}
+
+		if (mobile == null || mobile.trim().isEmpty()) {
+		    System.out.println("Mobile number is required");
+		    return;
+		}
+
+		if (password == null || password.trim().isEmpty()) {
+		    System.out.println("Password is required");
+		    return;
+		}
+		
+		//Validate Email id:
+		//[A-Za-z0-9]+([._-][A-Za-z0-9]+)*@ :ex->Reshma@
+		//[A-Za-z0-9]+: one or more 1st char accept only digits Or upper Or Lower case letters
+		//([._-][A-Za-z0-9]+)*: zero / more from 2nd it can be .,_,- ,any digit,alphabets,then one @
+		//[A-Za-z0-9]+(-[A-Za-z0-9]+)*: same like above ex:gmail 
+		//\\.[A-Za-z]{2,}: .atleast 2 characters-->ex:.com or .co.in etc
+		// *  → 0 or more  , +  → 1 or more , ?  → 0 or 1
+		 String regex =
+	                "^[A-Za-z0-9]+([._-][A-Za-z0-9]+)*@"
+	              + "[A-Za-z0-9]+(-[A-Za-z0-9]+)*"
+	              + "(\\.[A-Za-z]{2,})+$";
+
+	        if (email.matches(regex)) {
+	            System.out.println("Valid Email");
+	        } else {
+	            System.out.println("Invalid Email");
+	        }
+		
+		// Validate mobile number
+		if (!mobile.matches("[7-9][0-9]{9}")) {
+		    System.out.println("Invalid mobile number");
+		    return;
+		}
+		
+		//  Check password matching
+		if(password.matches(confirmPassword))
+			System.out.println("Password matched");
+		else
+			System.out.println("Password not matched");
+		
+		
 	
 	}
 
